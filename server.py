9e788107ea88dcf30cb9f3918b10760d80126f94
woomurf <dngusdnd@gmail.com>
2020-07-22 10:02:42 +0900

    healthCheck

--- server.py
+++ server.py
@@ -184,5 +184,9 @@ def preprocessing(input_text):
     
     return result
 
+@app.route("/healthz", methods = ["GET"])
+def healthCheck():
+    return "ok",200
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="80")
